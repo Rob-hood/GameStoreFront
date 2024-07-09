0e1a9757be8395edec9b033167275db9019553a6
rob1rob1@outlook.fr <rob1rob1@outlook.fr>
2024-07-09 10:03:38 +0200

    Page Mon Compte

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,9 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signin", "Se connecter", "/pages/signin.html"),
+    new Route("/account", "Mon espace", "/pages/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
